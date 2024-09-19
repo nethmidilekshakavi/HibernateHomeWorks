68da9ccd0da5767b23c3f5a6f963dc606054edec
nethmidilekshakavi <nethmidileksha6@gmail.com>
2024-09-19 11:51:32 +0530

    completed JPQL

--- JPQL/src/main/java/org/example/Main.java
+++ JPQL/src/main/java/org/example/Main.java
@@ -1,7 +1,4 @@
 package org.example;
-
-
-import Entity.Student;
 import config.FactoryConfiguration;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
@@ -18,56 +15,54 @@ public class Main {
                 Session session = FactoryConfiguration.getInstance().getSession();
                 Transaction transaction = session.beginTransaction();
 
-                //SELECT
+                //SELECT------------------------------------------------
 
-                Query query = session.createQuery("select s from Student s");
+              /*  Query query = session.createQuery("select s from Student s");
                 List<Student> studentList = query.getResultList();
 
                 for (Student student : studentList){
-                    System.out.println(student);
-                }
+                    System.out.println(student.getNo() + student.getName() + student.getMarks());
+                }*/
 
+                //UPDATE--------------------------------------------------------
 
-
-
-
-                //UPDATE
-
-        Query query1 = session.createQuery("update Student  s set s.name = :address, s.name = :name  where s.sId = :Id");
-        query1.setParameter("address","Galle");
-        query1.setParameter("name","Dilmi");
+/*
+        Query query1 = session.createQuery("update Student  s set s.marks = :marks, s.name = :name  where s.no = :Id");
+        query1.setParameter("marks","80");
+        query1.setParameter("name","maduri");
         query1.setParameter("Id",2);
         query1.executeUpdate();
+*/
 
 
-                //DELETE
+                //DELETE-------------------------------------------------------
 
-        /*Query query2 = session.createQuery("delete from Student s where s.sId = :Id");
+       /* Query query2 = session.createQuery("delete from Student s where s.no = :Id");
         query2.setParameter("Id",2);
         query2.executeUpdate();*/
 
 
-                //SEARCH BY ID
+                //SEARCH BY ID----------------------------------------------------
 
-        /*Query query3 = session.createQuery("select s from Student s where s.sId = :Id");
+       /* Query query3 = session.createQuery("select s from Student s where s.no = :Id");
         query3.setParameter("Id",3);
-        List<Student> stList = query3.list();
+        List<Student> stList = query3.getResultList();
 
         for (Student student : stList){
-            System.out.println(student);
+            System.out.println(student.getNo());
         }*/
 
 
-                //JOINQUERY
+                //JOIN QUERY-------------------------------------------------------
 
-        /*Query query4 = session.createQuery("select w from Student s join s.watchList w where s.sId = :Id");
+       /* Query query4 = session.createQuery("select l from Student s join s.laptop l where s.no = :Id");
         query4.setParameter("Id",1);
-        List<Watch> watches = query4.list();
+        List<Laptop> list = query4.getResultList();
 
-        for (Watch watch : watches){
-            System.out.println("Watch type :"+watch.getType());
-        }*/
-
+        for (Laptop laptop : list){
+            System.out.println("lap name :"+laptop.getLname());
+        }
+*/
                 transaction.commit();
                 session.close();
 }
